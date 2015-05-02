3e77e1bb022becbd4b41a74e45c780c06d814193
Wes Kendall <wes.kendall@ambition.com>
2015-05-02 21:04:36 +0000

    added 1.8 support and dropped 1.6 support

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 import os
 
-import django
 from django.conf import settings
 
 
@@ -34,6 +33,8 @@ def configure_settings():
             raise RuntimeError('Unsupported test DB {0}'.format(test_db))
 
         settings.configure(
+            TEST_RUNNER='django_nose.NoseTestSuiteRunner',
+            NOSE_ARGS=['--nocapture', '--nologcapture', '--verbosity=1'],
             MIDDLEWARE_CLASSES=(),
             DATABASES={
                 'default': db_config,
@@ -46,7 +47,7 @@ def configure_settings():
                 'entity',
                 'entity_event',
                 'entity_event.tests',
-            ) + (('south',) if django.VERSION[1] <= 6 else ()),
+            ),
             ROOT_URLCONF='entity_event.urls',
             DEBUG=False,
         )
