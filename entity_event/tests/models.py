19c9a8378b5ec4cd64eddab1b475403f217d2dee
Wes Okes <wes.okes@gmail.com>
2016-02-17 16:28:33 -0500

    nose fix

--- entity_event/tests/models.py
+++ entity_event/tests/models.py
@@ -2,14 +2,23 @@ from django.db import models
 
 
 class TestFKModel(models.Model):
+    # tell nose to ignore
+    __test__ = False
+
     value = models.CharField(max_length=64)
 
 
 class TestFKModel2(models.Model):
+    # tell nose to ignore
+    __test__ = False
+
     value = models.CharField(max_length=64)
 
 
 class TestModel(models.Model):
+    # tell nose to ignore
+    __test__ = False
+
     value = models.CharField(max_length=64)
     fk = models.ForeignKey(TestFKModel)
     fk2 = models.ForeignKey(TestFKModel2)
