f853e1374498774b3216e4c0d5031b92c096d932
Wes Okes <wes.okes@gmail.com>
2018-02-23 11:10:38 -0500

    testing new travis setup

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
